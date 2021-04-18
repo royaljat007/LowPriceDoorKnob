1de0bf05031c35ab352cb6ed2c9fa516753ad890
Inderjeet Choudhary <choudharyinderjeet0@gmail.com>
2021-04-18 18:17:28 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, BannerComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
