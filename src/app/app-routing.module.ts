95d8b47e186ade3e9eab4143ed5a1747899ec8e0
Inderjeet Choudhary <choudharyinderjeet0@gmail.com>
2021-04-20 12:26:40 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
-
+import { LoginComponent } from './pages/login/login.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'aboutus', component: AboutusComponent},
-  { path: 'contact_us', component: ContactUsComponent}
+  { path: 'contact_us', component: ContactUsComponent},
+  { path: 'login', component: LoginComponent}
   
 ];
 
